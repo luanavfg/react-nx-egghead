b406a44b5719fbb72c36ac836ffd6c15bb182bbc
Luana Vefago dos Santos <101883667+luanavfg@users.noreply.github.com>
2023-01-23 15:59:27 +0000

    feat: add game details on click

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -10,42 +10,49 @@ import  Typography  from "@material-ui/core/Typography";
 
 import {Header} from '@nxegghead/store/ui-shared'
 import {formatRating} from '@nxegghead/store/util-formatters'
+import { Route, useHistory } from "react-router-dom";
+
+import { StoreFeatureGameDetail } from '@nxegghead/store/feature-game-detail';
 
 export const App = () => {
+  const history = useHistory();
  return (
   <>
-  <Header/>
-  <div className="container">
-    <div className="games-layout">
-    {getAllGames().map((x) => (
-          <Card key={x.id} className="game-card">
-            <CardActionArea>
-              <CardMedia
-                className="game-card-media"
-                image={x.image}
-                title={x.name}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {x.name}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {x.description}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  component="p"
-                  className="game-rating"
-                >
-                  <strong>Rating:</strong> {formatRating(x.rating)}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        ))}
+    <Header/>
+    <div className="container">
+      <div className="games-layout">
+      {getAllGames().map((x) => (
+            <Card key={x.id} className="game-card"
+              onClick={() => history.push(`/game/${x.id}`)}
+            >
+              <CardActionArea>
+                <CardMedia
+                  className="game-card-media"
+                  image={x.image}
+                  title={x.name}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {x.name}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    {x.description}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                    className="game-rating"
+                  >
+                    <strong>Rating:</strong> {formatRating(x.rating)}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+      </div>
     </div>
-  </div>
+    <Route path="/game/:id" component={StoreFeatureGameDetail} />
   </>
  )
 };
