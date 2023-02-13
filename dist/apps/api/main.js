9b766c57f0aee415b10bc8caaa73fbfe3d0a1bc5
luanavfg <luanavefago1@gmail.com>
2023-02-13 08:32:59 -0300

    feat: add api folder project and add run-many command in workspace

---
+++ dist/apps/api/main.js
@@ -0,0 +1,190 @@
+(function(e, a) { for(var i in a) e[i] = a[i]; }(exports, /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./apps/api/src/app/games.repository.ts":
+/*!**********************************************!*\
+  !*** ./apps/api/src/app/games.repository.ts ***!
+  \**********************************************/
+/*! exports provided: getAllGames, getGame */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "getAllGames", function() { return getAllGames; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "getGame", function() { return getGame; });
+const games = [
+    {
+        id: 'settlers-in-the-can',
+        name: 'Settlers in the Can',
+        image: '/assets/beans.png',
+        description: 'Help your bug family claim the best real estate in a spilled can of beans.',
+        price: 35,
+        rating: Math.random(),
+    },
+    {
+        id: 'chess-pie',
+        name: 'Chess Pie',
+        image: '/assets/chess.png',
+        description: 'A circular game of Chess that you can eat as you play.',
+        price: 15,
+        rating: Math.random(),
+    },
+    {
+        id: 'purrfection',
+        name: 'Purrfection',
+        image: '/assets/cat.png',
+        description: 'A cat grooming contest goes horribly wrong.',
+        price: 45,
+        rating: Math.random(),
+    },
+];
+const getAllGames = () => games;
+const getGame = (id) => games.find((game) => game.id === id);
+
+
+/***/ }),
+
+/***/ "./apps/api/src/main.ts":
+/*!******************************!*\
+  !*** ./apps/api/src/main.ts ***!
+  \******************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var express__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! express */ "express");
+/* harmony import */ var express__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(express__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _app_games_repository__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./app/games.repository */ "./apps/api/src/app/games.repository.ts");
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+
+const app = express__WEBPACK_IMPORTED_MODULE_0__();
+app.get('/api/games', (req, res) => {
+    res.send(Object(_app_games_repository__WEBPACK_IMPORTED_MODULE_1__["getAllGames"])());
+});
+app.get('/api/games/:id', (req, res) => {
+    return res.send(Object(_app_games_repository__WEBPACK_IMPORTED_MODULE_1__["getGame"])(req.params.id));
+});
+const port = process.env.port || 3333;
+const server = app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
+
+
+/***/ }),
+
+/***/ 0:
+/*!************************************!*\
+  !*** multi ./apps/api/src/main.ts ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! /home/luana.santos/Documentos/GitHub/react-nx-egghead/apps/api/src/main.ts */"./apps/api/src/main.ts");
+
+
+/***/ }),
+
+/***/ "express":
+/*!**************************!*\
+  !*** external "express" ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("express");
+
+/***/ })
+
+/******/ })));
+//# sourceMappingURL=main.js.map
